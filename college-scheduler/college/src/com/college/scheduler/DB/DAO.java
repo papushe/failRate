109cd7b7ce2ef9a12107c3818ebd5cc36d45fe14
Amit Papushe Shely <papushe9@gmail.com>
2017-01-04 08:44:12 +0200

    Delete DAO.java

--- college-scheduler/college/src/com/college/scheduler/DB/DAO.java
+++
@@ -1,9 +0,0 @@
-package com.college.scheduler.DB;
-import java.sql.Connection;
-
-public interface DAO {
-	public Connection createConection();
-	public void closeConection();
-	public void generateDefaultData();
-	public void dropTables();
-}
